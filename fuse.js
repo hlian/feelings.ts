185350a6b5cee1c5980b76d7451d30d5e46345b2
Hao Lian <hi@haolian.org>
2019-07-18 20:52:32 +0000

    execstart

--- fuse.js
+++ fuse.js
@@ -1,18 +1,43 @@
-const { FuseBox } = require('fuse-box');
-const fuse = FuseBox.init({
-  useTypescriptCompiler: true,
-  experimentalFeatures: true,
-  homeDir: 'server',
-  target: 'server',
-  output: 'dist/$name.js',
-  alias: {
-    server: '~',
-  },
+const { FuseBox, QuantumPlugin } = require('fuse-box');
+const { context, task } = require('fuse-box/sparky');
+
+context(
+  class {
+    getConfig() {
+      return FuseBox.init({
+        useTypescriptCompiler: true,
+        experimentalFeatures: true,
+        homeDir: 'server',
+        target: 'server',
+        output: 'dist/$name.js',
+        plugins: [
+          this.isProduction &&
+            QuantumPlugin({
+              target: 'server',
+              bakeApiIntoBundle: 'server',
+            }),
+        ],
+        alias: {
+          server: '~',
+        },
+      });
+    }
+  }
+);
+
+task('default', context => {
+  const fuse = context.getConfig();
+  fuse
+    .bundle('server')
+    .watch('server/**')
+    .instructions(' > [_entry.ts]')
+    .completed(proc => proc.start());
+  fuse.run();
 });
 
-fuse
-  .bundle('server')
-  .watch('server/**')
-  .instructions(' > [_entry.ts]')
-  .completed(proc => proc.start());
-fuse.run();
+task('dist', context => {
+  context.isProduction = true;
+  const fuse = context.getConfig();
+  fuse.bundle('server').instructions(' > [_entry.ts]');
+  fuse.run();
+});
